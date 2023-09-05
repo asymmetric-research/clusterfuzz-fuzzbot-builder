6f17ddd3db8ef380a985244853783d25843b91f3
Marcus <117692400+marcus-jump@users.noreply.github.com>
2023-09-05 15:08:56 +0000

    add kernel headers

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,7 @@ set -exo pipefail
 
 
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y git build-essential lsb-release wget software-properties-common gnupg
+DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y git build-essential lsb-release wget software-properties-common gnupg linux-headers-5.8.0-63-generic
 cd /usr/bin
 
 bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
