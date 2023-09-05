f20f9e7c5c738dee816396ccdb7a9c8a17475993
Marcus <117692400+marcus-jump@users.noreply.github.com>
2023-09-05 19:20:08 +0000

    pick more recent linux headers and link dir

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,6 @@ COPY scripts /scripts/
 RUN ls -lah /scripts/
 RUN /scripts/setup.sh
 
+ENV EXTRA_CPPFLAGS="-isystem /usr/src/linux-hwe-5.15-headers-5.15.0-56/include"
+
 ENTRYPOINT ["/scripts/entrypoint.sh"]
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,9 +4,8 @@ set -exo pipefail
 
 
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y git build-essential lsb-release wget software-properties-common gnupg linux-headers-5.8.0-63-generic
+DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y git build-essential lsb-release wget software-properties-common gnupg linux-headers-5.15.0-56-generic
 cd /usr/bin
-
 bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
 
 ln -s ../lib/llvm-17/bin/clang clang
