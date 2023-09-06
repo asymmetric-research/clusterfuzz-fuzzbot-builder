716154aea1b52a4a18606afa36907a2d4f61272a
Marcus <117692400+marcus-jump@users.noreply.github.com>
2023-09-06 17:41:42 +0000

    update to 22.04

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
-FROM ubuntu:20.04
+FROM ubuntu:22.04
 
 COPY scripts /scripts/
 RUN ls -lah /scripts/
 RUN /scripts/setup.sh
-
-ENV EXTRA_CPPFLAGS="-isystem /usr/src/linux-hwe-5.15-headers-5.15.0-56/include"
 
 ENTRYPOINT ["/scripts/entrypoint.sh"]
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,11 +4,9 @@ set -exo pipefail
 
 
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y git build-essential lsb-release wget software-properties-common gnupg linux-headers-5.15.0-56-generic
+DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y git build-essential lsb-release wget software-properties-common gnupg linux-headers-generic
 cd /usr/bin
 bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
 
 ln -s ../lib/llvm-17/bin/clang clang
 ln -s ../lib/llvm-17/bin/clang++ clang++
-rm -r /usr/include/x86_64-linux-gnu/
-
