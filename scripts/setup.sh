faec3e067f15da42e59bce82747d55a78b15be04
Marcus <117692400+marcus-jump@users.noreply.github.com>
2023-09-08 15:23:43 +0000

    add librocksdb-dev

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,15 @@ set -exo pipefail
 
 
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y git build-essential lsb-release wget software-properties-common gnupg linux-headers-generic
+DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
+  git \
+  build-essential \
+  lsb-release wget \
+  software-properties-common \
+  gnupg \
+  linux-headers-generic \
+  librocksdb-dev
+
 cd /usr/bin
 bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
 
