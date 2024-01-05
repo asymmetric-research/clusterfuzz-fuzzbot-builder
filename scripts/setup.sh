12f4ebc308b4e7340b886824ed9159546e6317c8
ripatel-fd <ripatel+git@jumptrading.com>
2024-01-05 16:29:57 +0100

    Create LICENSE
    fix the llvm version to 17
    
    added custom llvm
    
    use curl instead of wget
    
    add unzip
    
    use published docker image instead of rebuilding
    
    testing

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,6 @@
 
 set -exo pipefail
 
-
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
   curl \
@@ -12,10 +11,13 @@ DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
   software-properties-common \
   gnupg \
   linux-headers-generic \
-  librocksdb-dev
+  librocksdb-dev \
+  unzip
 
 cd /usr/bin
-bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+
+curl -o llvmorg-17.0.6.zip https://fuzzcorp-dist-a9302fb.s3.us-east-2.amazonaws.com/llvmorg-17.0.6.zip
+unzip llvmorg-17.0.6.zip
 
 # install rust
 curl https://sh.rustup.rs -sSf | sh -s -- -y
@@ -24,9 +26,9 @@ rustup toolchain install nightly
 rustup default nightly
 rustup component add rust-src --toolchain nightly-x86_64-unknown-linux-gnu
 
-ln -s ../lib/llvm-17/bin/clang clang
-ln -s ../lib/llvm-17/bin/clang++ clang++
+ln -s "/usr/bin/llvmorg-17.0.6/bin/clang" clang
+ln -s "/usr/bin/llvmorg-17.0.6/bin/clang++" clang++
 
-ln -sf /usr/bin/ld.lld-17 /usr/bin/ld
-update-alternatives --install /usr/bin/cc cc /lib/llvm-17/bin/clang 50
-update-alternatives --install /usr/bin/c++ c++ /lib/llvm-17/bin/clang++ 50
+ln -sf //usr/bin/llvmorg-17.0.6/bin/ld.lld /usr/bin/ld
+update-alternatives --install /usr/bin/cc cc "/usr/bin/llvmorg-17.0.6/bin/clang" 50
+update-alternatives --install /usr/bin/c++ c++ "/usr/bin/llvmorg-17.0.6/bin/clang++" 50
